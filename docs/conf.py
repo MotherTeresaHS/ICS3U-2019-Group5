6cc82af5df0b09a22f739f2325932c4ff2168e28
jack-dangelo <54954654+jack-dangelo@users.noreply.github.com>
2020-01-17 13:07:14 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Clown Town - CircuitPython Game'
-copyright = u'2020, Teddy Sannan & Jack D'Angelo'
+copyright = u'2020, Teddy Sannan & Jack Dangelo'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
